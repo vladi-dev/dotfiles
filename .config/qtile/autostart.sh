4179c3faa7157b6412ac88fb0e55b581cc3c24aa
vladi-dev <vladi-dev@users.noreply.github.com>
2022-01-15 16:07:33 -0800

    Second commit

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+exec 1>/tmp/autostart.log 2>&1
+
+runCommand() {
+    pkill "$(echo "$@" | cut -d ' ' -f 1)"
+    ("$@" > /tmp/"$1.log" 2>&1) &
+}
+
+runCommand feh --bg-scale ~/Pictures/Wallpapers/wall.png
+runCommand conky
