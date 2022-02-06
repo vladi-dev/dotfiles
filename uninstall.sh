8701752f27d275b346ab254487c71ebd08b6789b
vladi-dev <vladi-dev@users.noreply.github.com>
2022-02-05 23:58:37 -0800

    add libinput-gestures

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-stow -D -v 1 --dotfiles -t ~ home/ alacritty/ qtile/ ranger/ systemd/ kitty/ zsh/ picom/
+stow -D -v 1 --dotfiles -t ~ home/ alacritty/ qtile/ ranger/ systemd/ kitty/ zsh/ picom/ libinput-gestures/
 
